9372b1300ad049666acb19ac52029e1c7dcb134e
divyanshu <divyanshusah44@gmail.com>
2026-07-30 23:30:29 +0530

    learned components

---
+++ complete-react/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./components/card"
+
+const App = () => {
+  return (
+   <>
+      <Card/>
+      
+   </>
+    
+  )
+}
+
+export default App
---
+++ complete-react/src/components/card.jsx
@@ -0,0 +1,15 @@
+
+import "./card.css"
+
+const Card = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>divi</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem provident neque ratione numquam aperiam quod, magnam porro fugiat iusto repellendus.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
