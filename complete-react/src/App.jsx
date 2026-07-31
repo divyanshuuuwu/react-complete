880429d1eeaef9643323799c18e34ad033797cf7
divyanshu <divyanshusah44@gmail.com>
2026-07-31 16:12:31 +0530

    created the card

--- complete-react/src/App.jsx
+++ complete-react/src/App.jsx
@@ -1,12 +1,11 @@
-import Card from "./components/card"
+import React from 'react'
+import Card from './components/Card'
 
 const App = () => {
   return (
-   <div className="parent">
-    <Card user="aman"/>
-    <Card user="divyanshu"/>
-   </div>
-    
+    <div className='parent'>
+      <Card />
+       </div>
   )
 }
 
--- complete-react/src/components/card.jsx
+++ complete-react/src/components/card.jsx
@@ -1,13 +1,24 @@
+import React from 'react'
+import "./Card.css"
 
-import "./card.css"
-
-const Card = (props) => {
-  console.log(props)
+const Card = () => {
   return (
-    <div>
-      <div className="card">
-        <h1>{props.user}</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem provident neque ratione numquam aperiam quod, magnam porro fugiat iusto repellendus.</p>
+    <div className='card'>
+      <div className="top">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/3840px-Google_%22G%22_logo.svg.png" alt="" />
+        <button className='savebtn'>save</button>
+      </div>
+      <div className="center">
+        <h3>Google</h3>
+        <h1>Software Dev</h1>
+        <span>Part-Time</span> <span>Flexible Schedule</span>
+      </div>
+      <div className="bottom">
+        <div className="des">
+          <h3>$100k-150k</h3>
+        <h5>Delhi, India</h5>
+        </div>
+        <button className="applynow">Apply now</button>
       </div>
     </div>
   )
