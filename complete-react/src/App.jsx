19b42b7a1f2ada87a864af429c69824c204a490d
divyanshu <divyanshusah44@gmail.com>
2026-07-31 16:26:55 +0530

    ui complete

--- complete-react/src/App.jsx
+++ complete-react/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Card from './components/Card'
+import "./App.css";
 
 const App = () => {
   return (
-    <div className='parent'>
+    <div className='cards-container'>
       <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+
+
        </div>
   )
 }
